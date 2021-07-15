0470e49693e09ce69bda736caf5dd0225d1097c9
Peter Fong <pefong@microsoft.com>
2021-07-14 23:33:23 -0700

    Clean state

--- interview/AppDelegate.swift
+++ interview/AppDelegate.swift
@@ -10,10 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    let rootViewController = InitialViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = rootViewController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
